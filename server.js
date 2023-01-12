ff1c07c7ab16afb45cf952b40e37ea6eb82b15da
Taylor Remund <remundtaylor3@gmail.com>
2023-01-12 16:23:20 -0700

    added rollbar

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar');
+var rollbar = new Rollbar({
+  accessToken: '640e85fdf1e045249b82d04f805115a0',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+});
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!');
+
 app.use(express.static(`${__dirname}/public`));
 
 app.listen(4000, () => console.log('up on 4000'));
